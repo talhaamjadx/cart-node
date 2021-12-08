58cb0c367c90f582c6ba9ea50650e3ca94466939
talha789 <talha.amjad@techandover.com>
2021-12-08 17:21:31 +0500

    cart added

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,3 +1,66 @@
+const Book = require("../models/booksModel")
+
 exports.getCartController = (req, res, next) => {
-    res.render('cart')
+    req.user.getCart()
+        .then(cart => {
+            return cart.getBooks()
+        })
+        .then(books => {
+            res.render('cart', { books: books })
+        })
+        .catch(err => {
+            res.send(err)
+        })
+}
+
+exports.cartItemRemoveController = (req, res, next) => {
+    let fetchedCart;
+    req.user.getCart()
+        .then(cart => {
+            fetchedCart = cart
+            return cart.removeBook(req.params.id)
+        })
+        .then(() => {
+            return fetchedCart.getBooks()
+        })
+        .then(books => {
+            res.render('cart', { books: books })
+        })
+        .catch(err => {
+            res.send(err)
+        })
+}
+
+exports.addBookToCartController = (req, res, next) => {
+    const bookId = req.params.id
+    let fetchedCart;
+    req.user.getCart()
+        .then(cart => {
+            fetchedCart = cart
+            return cart.getBooks({ where: { id: bookId } })
+        })
+        .then(books => {
+            let quantity;
+            if (books[0]) {
+                let book = books[0]
+                quantity = book.cartbook.quantity
+                return fetchedCart.addBook(book, { through: { quantity: quantity + 1 } })
+            }
+            else {
+                return Book.findByPk(bookId)
+                    .then(book => {
+                        return fetchedCart.addBook(book, { through: { quantity: 1 } })
+                    })
+                    .then(() => {
+                        res.redirect('/cart')
+                    })
+                    .catch(err => {
+                        res.send(err)
+                    })
+            }
+        })
+        .then(() => {
+            res.redirect('/cart')
+        })
+        .catch(err => res.send(err))
 }
--- routes/cart.js
+++ routes/cart.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getCartController } = require("../controllers/cart")
+const { getCartController, addBookToCartController, cartItemRemoveController } = require("../controllers/cart")
 
 router.get('/cart', getCartController)
+
+router.post('/cart/add/:id', addBookToCartController)
+
+router.post('/cart/remove-item/:id', cartItemRemoveController)
 
 module.exports = router;
